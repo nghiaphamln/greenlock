e6875515e23bc529e024c4b6395bbfe79e421166
NghiaPH <NghiaPH>
2020-05-19 02:21:13 +0700

    edit diem

--- app.js
+++ app.js
@@ -196,7 +196,7 @@ const hocbong =
     "1. Đối tượng được cấp, xét học bổng\n- Sinh viên hệ chính quy được xếp loại học tập và rèn luyện trong học kỳ xét học bổng từ loại khá trở lên, không bị xử lí kỷ luật với bất kì hình thức nào\n- Sinh viên thuộc diện hưởng học bổng chính sách, trợ cấp xã hội và chính sách ưu đãi theo quy định hiện hành thì ngoài chế độ đang được hưởng, nếu đạt kết quả xếp loại học tập và rèn luyện trong học kì từ khá trở lên cũng được xét, cấp học bổng như các sinh viên khác\n2. Tiêu chuẩn xếp loại\n- Xuất sắc:\n+ Điểm từ 9 đến 10\n+ Kết quả rèn luyện loại xuất sắc\n- Giỏi:\n+ Đạt điểm từ 8 đến cận 9\n+ Kết quả rèn luyện loại tốt trở lên\n- Khá:\n+ Đạt điểm từ 7 đến cận 8\n+ Kết quả rèn luyện loại khá trở lên";
 
 
-const diem = await TDMU.getAllMark('1824801040118', '123456789');
+const diem = TDMU.getAllMark('1824801040118', '123456789');
 if (require.main === module) {
   app.listen(3000);
 }
