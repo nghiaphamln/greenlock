6cd81f83ec1e2e641115f6bb9cfcb9c038cdb0e8
NghiaPH <NghiaPH>
2020-05-18 16:22:55 +0700

    edit server.js

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 "use strict";
 
-const APP_SECRET = "abe11a2c4f36b4be64497e4025f6a065";
-const VALIDATION_TOKEN = "minh-nghia";
-const PAGE_ACCESS_TOKEN = "EAAD7RaVAYZAIBAONSrFoQVPkXQ2dyDfRZC5MXYqipq1jiBQ3ZBdUZArf0SZA0cNY6US49y6H37uohvQ0tA864ry1PtNENjRQuk2tVxlCrVe9ZCHRRoHekSDLEJu4cOpt4d1JTnuFfeNlnthJFxZAdTO07fW3mvMlZApBZCFSjC0dQqJYgoD54CLBZCE6wUsSEVkesZD";
-
 var apps = require("./app.js");
 
 require("greenlock-express")
@@ -17,75 +13,66 @@ require("greenlock-express")
   })
   .serve(apps);
 
-var http = require("http");
-var bodyParser = require("body-parser");
-var express = require("express");
-
+  var logger = require('morgan');
+var http = require('http');
+var bodyParser = require('body-parser');
+var express = require('express');
+var request = require('request');
+var router = express();
 var app = express();
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  })
-);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 var server = http.createServer(app);
-var request = require("request");
-
-app.get("/", (req, res) => {
-  res.send("Home page. Server running okay.");
+app.listen(process.env.PORT || 3000);
+app.get('/', (req, res) => {
+  res.send("Server chạy ngon lành.");
 });
-
-app.get("/webhook", function (req, res) {
-  // Đây là path để validate tooken bên app facebook gửi qua
-  if (req.query["hub.verify_token"] === VALIDATION_TOKEN) {
-    res.send(req.query["hub.challenge"]);
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.verify_token'] === 'minh-nghia') {
+    res.send(req.query['hub.challenge']);
   }
-  res.send("Error, wrong validation token");
+  res.send('Error, wrong validation token');
 });
-
-app.post("/webhook", function (req, res) {
-  // Phần sử lý tin nhắn của người dùng gửi đến
+// Đoạn code xử lý khi có người nhắn tin cho bot
+app.post('/webhook', function(req, res) {
   var entries = req.body.entry;
   for (var entry of entries) {
     var messaging = entry.messaging;
     for (var message of messaging) {
       var senderId = message.sender.id;
       if (message.message) {
+        // Nếu người dùng gửi tin nhắn đến
         if (message.message.text) {
           var text = message.message.text;
-          sendMessage(senderId, "Hello!! I'm a bot. Your message: " + text);
+          if(text == 'hi' || text == "hello")
+          {
+            sendMessage(senderId, "Trung Quân's Bot: " + 'Xin Chào');
+          }
+          else{sendMessage(senderId, "Trung Quân's Bot: " + "Xin lỗi, câu hỏi của bạn chưa có trong hệ thống, chúng tôi sẽ cập nhật sớm nhất.");}
         }
       }
     }
   }
   res.status(200).send("OK");
 });
-
-// Đây là function dùng api của facebook để gửi tin nhắn
+// Gửi thông tin tới REST API để Bot tự trả lời
 function sendMessage(senderId, message) {
   request({
-    url: "https://graph.facebook.com/v2.6/me/messages",
+    url: 'https://graph.facebook.com/v2.6/me/messages',
     qs: {
-      access_token: PAGE_ACCESS_TOKEN,
+      access_token: "EAAD7RaVAYZAIBAP5FDle5shD6iBQrjgqarZCKduKX11q1Ng07gyKWZAwOjMwgh7UYJivu7uGHQA2wOk8gzSLFeVaPIelBVoY5AktBcXrUZB236AuGMKDANOqa4FF05KB9xZAR0BFX5NSeVCfMUlPyZBFdMtIJhWPOZBnf3kFpHA9IZCsjsaT22H1bhgQFHTKvF8ZD",
     },
-    method: "POST",
+    method: 'POST',
     json: {
       recipient: {
-        id: senderId,
+        id: senderId
       },
       message: {
-        text: message,
+        text: message
       },
-    },
+    }
   });
 }
-
-app.set("port", process.env.PORT || 5000);
-app.set("ip", process.env.IP || "0.0.0.0");
-
-server.listen(app.get("port"), app.get("ip"), function () {
-  console.log(
-    "Chat bot server listening at %s:%d ",
-    app.get("ip"),
-    app.get("port")
-  );
-});
